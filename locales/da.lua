448a6a9857dac7dc4caaf8db0bf0ffe1ec3c5010
Tom <111147401+1OSaft@users.noreply.github.com>
2024-04-22 07:45:29 +0200

    Added danish
    Thanks to Chifu

---
+++ locales/da.lua
@@ -0,0 +1,78 @@
+Locales ['da'] = {
+    ['LabLocked'] = 'Du kan ikke komme ind i dette laboratorium, det er låst',
+    ['BoughtLab'] = 'Du har lige købt meth-laboratoriet',
+    ['CantBuy'] = 'Du kunne ikke købe dette laboratorium!',
+
+----------------------------------------------------------------
+    -- Generelt
+    ['MissingResources'] = 'Du mangler: ',
+    ['ToMuchLabsBought'] = 'Du har overskredet grænserne for meth-laboratorier',
+    ['CanceledProduction'] = 'Produktion afbrudt',
+----------------------------------------------------------------
+
+    -- Raidtekst
+    ['SuccessfullyRaided'] = 'Du har succesfuldt klippet låsen, du kan nu komme ind',
+    ['FailedRaid'] = 'Du idiot ødelagde værktøjet',
+    ['CantRaid'] = 'Dette laboratorium kan ikke overfalde i øjeblikket, prøv igen senere',
+    ['CantLockWhileRaid'] = 'Du kan ikke bare låse laboratoriet, mens det bliver overfaldet',
+
+    ['BlipText'] = 'Dit laboratorium bliver i øjeblikket overfaldet',
+
+----------------------------------------------------------------
+    -- AlertDialog
+    ['AlertDialogHeader'] = 'Køb meth-laboratorium',
+    ['AlertDialogHeaderDesc'] = 'Vil du købe dette laboratorium?',
+
+----------------------------------------------------------------
+    -- TextUI
+    ['PouringTextUI'] = '[E] Hæld kemikalier',
+    ['RefineryTextUI'] = '[E] Raffiner kemikalie-slam',
+    ['NormalMenuTextUI'] = '[E] Åbn meth-laboratorium-menuen',
+    ['StorageTextUI'] = '[E] Åbn meth-lager',
+
+----------------------------------------------------------------
+    -- InputDialog
+    ['RecipeDialogHeader'] = 'Kemikalieblander',
+    ['SelectRecipeDialog'] = 'Vælg meth-oppskrift',
+    ['SelectRecipeDialogDesc'] = '',
+
+    ['SlurryDialogHeader'] = 'Hærder',
+    ['SelectSlurryRecipeDialog'] = 'Vælg slam-oppskrift',
+    ['SelectSlurryRecipeDialogDesc'] = '',
+
+----------------------------------------------------------------
+    -- Progressbar
+    ['ChemicalPouringProgress'] = 'Hæld kemikalier',
+    ['SlurryRefineryProgress'] = 'Kontroller temperatur',
+    ['RaidProgress'] = 'Skær låsen over...',
+
+----------------------------------------------------------------
+    -- Menus
+    ['EnterContextmarker'] = 'Meth-laboratorium-menu',
+
+    ['EnterLabel'] = 'Gå ind i laboratoriet',
+    ['EnterLabelDesc'] = 'Gå ind i dette meth-laboratorium',
+
+    ['LockLabel'] = 'Lås laboratoriet',
+    ['LockLabelDesc'] = 'Lås dette meth-laboratorium',
+    ['CantLockLab'] = 'Du kan ikke låse et laboratorium uden nøglen!',
+    ['UnlockedLab'] = 'Laboratoriet er ulåst',
+    ['LockedLab'] = 'Laboratoriet er låst',
+
+    ['RaidLabel'] = 'Overfald laboratoriet',
+    ['RaidLabelDesc'] = 'Overfald dette meth-laboratorium',
+
+    ['LeaveLab'] = 'Forlad laboratoriet',
+    ['LeaveLabDesc'] = 'Forlad dette meth-laboratorium',
+
+    ['UpgradeLab'] = 'Opgrader laboratoriet',
+    ['UpgradeLabDesc'] = 'Opgrader dette meth-laboratorium',
+
+    ['UpgradeStorage'] = 'Opgrader lageret',
+    ['UpgradedStorage'] = 'Dit lager er blevet opgraderet succesfuldt',
+    ['UpgradeSecurity'] = 'Opgrader sikkerheden',
+    ['UpgradedSecurity'] =  'Din sikkerhed er blevet opgraderet succesfuldt',
+    ['CurrentLevel'] = 'Nuværende niveau: ',
+}
+
+-- Translated by: Chifu
