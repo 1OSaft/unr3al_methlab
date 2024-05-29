8ae9366f183dc13e97edaa221ad3cac8662c4a03
Kaasklontje <84289148+Kaasklontje@users.noreply.github.com>
2024-05-29 22:51:57 +0200

    Create nl.lua

---
+++ locales/nl.lua
@@ -0,0 +1,65 @@
+Locales ['nl'] = {
+    ['LabLocked'] = 'Je kunt deze lab betreden, het is vergrendeld',
+    ['BoughtLab'] = 'Je hebt zojuist het methlab gekocht',
+    ['CantBuy'] = 'Je kon deze lab niet kopen!',
+
+    ['MissingResources'] = 'Je mist: ',
+    ['ToMuchLabsBought'] = 'Je hebt de limieten voor methlabs overschreden',
+    ['CanceledProduction'] = 'Productie geannuleerd',
+    
+    ['SuccessfullyRaided'] = 'Je hebt het slot met succes doorgeknipt, je kunt nu naar binnen',
+    ['FailedRaid'] = 'Je hebt het gereedschap kapot gemaakt, sukkel',
+    ['CantRaid'] = 'Deze lab kan momenteel niet worden overvallen, probeer het later opnieuw',
+    ['CantLockWhileRaid'] = 'Je kunt de lab niet vergrendelen terwijl hij wordt overvallen',
+    
+    ['BlipText'] = 'Je lab wordt momenteel overvallen',
+    
+    ['AlertDialogHeader'] = 'Methlab kopen',
+    ['AlertDialogHeaderDesc'] = 'Wil je deze lab kopen?',
+    ['AlertDialogHeaderBuy'] = 'Wie moet deze lab bezitten?',
+    ['BuyOptionPlayer'] = 'Ik ben de eigenaar',
+    ['BuyOptionSociety'] = 'Mijn gang is de eigenaar',
+    
+    ['PouringTextUI'] = '[E] Giet chemicaliën',
+    ['RefineryTextUI'] = '[E] Verfijn chemische slurry',
+    ['NormalMenuTextUI'] = '[E] Open methlab menu',
+    ['StorageTextUI'] = '[E] Open methlab opslag',
+    
+    ['RecipeDialogHeader'] = 'Chemische mixer',
+    ['SelectRecipeDialog'] = 'Selecteer meth recept',
+    ['SelectRecipeDialogDesc'] = '',
+    
+    ['SlurryDialogHeader'] = 'Verdikker',
+    ['SelectSlurryRecipeDialog'] = 'Selecteer slurry recept',
+    ['SelectSlurryRecipeDialogDesc'] = '',
+    
+    ['ChemicalPouringProgress'] = 'Chemicaliën gieten',
+    ['SlurryRefineryProgress'] = 'Temperatuur controleren',
+    ['RaidProgress'] = 'Slot doorzagen...',
+    
+    ['EnterContextmarker'] = 'Methlab menu',
+    
+    ['EnterLabel'] = 'Betreed lab',
+    ['EnterLabelDesc'] = 'Betreed deze methlab',
+    
+    ['LockLabel'] = 'Vergrendel lab',
+    ['LockLabelDesc'] = 'Vergrendel deze methlab',
+    ['CantLockLab'] = 'Je kunt een lab niet vergrendelen zonder sleutel!',
+    ['UnlockedLab'] = 'Lab ontgrendeld',
+    ['LockedLab'] = 'Lab vergrendeld',
+    
+    ['RaidLabel'] = 'Overval lab',
+    ['RaidLabelDesc'] = 'Overval deze methlab',
+    
+    ['LeaveLab'] = 'Verlaat lab',
+    ['LeaveLabDesc'] = 'Verlaat deze methlab',
+    
+    ['UpgradeLab'] = 'Upgrade methlab',
+    ['UpgradeLabDesc'] = 'Upgrade deze methlab',
+    
+    ['UpgradeStorage'] = 'Opslag upgrade',
+    ['UpgradedStorage'] = 'Opslag succesvol geüpgraded',
+    ['UpgradeSecurity'] = 'Beveiliging upgrade',
+    ['UpgradedSecurity'] =  'Beveiliging succesvol geüpgraded',
+    ['CurrentLevel'] = 'Huidig niveau: ',
+}
